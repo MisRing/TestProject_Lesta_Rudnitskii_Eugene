aae8c537c0fc5634cc72304400ba40f7973cf8e7
MisRing <kamandordark@gmail.com>
2024-09-12 22:35:26 +0300

    Timer and Damage

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController instance;
+
+    [SerializeField]
+    private int maxHealth = 10;
+    [SerializeField]
+    private int currentHealth = 10;
+
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public Transform cameraTransform;
@@ -12,13 +19,21 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        instance = this;
+
         rb = GetComponent<Rigidbody>();
+        currentHealth = maxHealth;
+
+        UIController.instance.ChangeHealthBar(currentHealth, maxHealth);
     }
 
     void Update()
     {
         MovePlayer();
         Jump();
+
+        if (transform.position.y < -5)
+            TakeDamage(99999);
     }
 
     void MovePlayer()
@@ -37,6 +52,14 @@ public class PlayerController : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
         }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = currentHealth < 0 ? 0 : currentHealth;
+
+        UIController.instance.ChangeHealthBar(currentHealth, maxHealth);
     }
 
     void Jump()
---
+++ Assets/UIController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    [SerializeField]
+    private RectTransform healthBar;
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private Text timerText;
+
+    [SerializeField]
+    private float seconds;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void StartRun()
+    {
+        StartCoroutine(Timer());
+    }
+
+    public void EndRun()
+    {
+        StopAllCoroutines();
+    }
+
+
+    public void ChangeHealthBar(int currentHealth, int maxHealth)
+    {
+        healthBar.localScale = new Vector3((float)currentHealth / maxHealth, 1, 1);
+        healthText.text = currentHealth.ToString() + "/" + maxHealth.ToString();
+    }
+
+    public IEnumerator Timer()
+    {
+        while (true)
+        {
+            yield return null;
+            seconds += Time.deltaTime;
+
+            string sec = ((int)seconds).ToString();
+            sec = sec.Length > 1 ? sec : "0" + sec;
+
+            int msecInt = (int)((seconds - (int)seconds) * 100);
+            string msec = msecInt < 10 ? "0" + msecInt.ToString() : msecInt.ToString();
+
+            timerText.text = sec + ":" + msec;
+        }
+        
+    }
+}
